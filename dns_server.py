23f4d5398ccaa681258ec4cbb83dda4e4c409bbd
Poya <amini.poya83@gmail.com>
2025-12-15 11:53:58 +0330

    Now DNS asks the database for the IP address.

--- dns_server.py
+++ dns_server.py
@@ -1,30 +1,52 @@
 import socket
-from dnslib import DNSRecord, RR, A, QTYPE
-
+from dnslib import DNSRecord, RR, A, QTYPE, RCODE
+import sqlite3
 port = 53
+db_file = "dns.db"
+
+createTableSql = """
+CREATE TABLE IF NOT EXISTS records (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    domain TEXT UNIQUE,
+    ip TEXT
+)
+"""
+selectDataSql = """
+SELECT ip FROM records WHERE domain = ?
+"""
+
+conn = sqlite3.connect(db_file)
+cursor = conn.cursor()
+cursor.execute(createTableSql)
+conn.commit()
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(("127.0.0.1",port))
+sock.bind(("0.0.0.0", port))
+
 print(f"DNS Server running on port {port} ...")
 
 while True:
-    data, addr = sock.recvfrom(512)
+    data , address = sock.recvfrom(512)
 
     request = DNSRecord.parse(data)
     reply = request.reply()
 
-    qname = request.q.qname
+    domain = str(request.q.qname).rstrip(".").lower()
     qtype = QTYPE[request.q.qtype]
 
-    print(f"Query received: {qname} ({qtype})")
+    print(f"Query from {address[0]} -> {domain} ({qtype})")
 
-    reply.add_answer(
-        RR(
-            rname=qname,
-            rtype=QTYPE.A,
-            rclass=1,
-            ttl=60,
-            rdata=A("127.0.0.1")
-        )
-    )
+    if qtype == "A": 
+        cursor.execute(selectDataSql,(domain,))
+        result = cursor.fetchone()
 
-    sock.sendto(reply.pack(), addr)
+        if result is not None:
+            ip = result[0]
+            reply.add_answer(
+                RR(domain, QTYPE.A, rdata=A(ip))
+            )
+        else: reply.header.rcode =  RCODE.NXDOMAIN
+    else:
+        reply.header.rcode = RCODE.NOTIMP
+
+    sock.sendto(reply.pack(), address)
